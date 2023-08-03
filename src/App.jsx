0ed669d8715c5a741204fbe320450408cee9967d
JonibekD <jonibekavazov18356@gmail.com>
2023-08-03 10:57:09 +0500

    queryId

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,7 @@ const App = () => {
         body: JSON.stringify(cartItems),
       })
     } else{
-      telegram.sendData(JSON.stringify(cartItems));
+      telegram.sendData(JSON.stringify({products: cartItems, queryId: queryId}));
     }
   }, [cartItems]);
 
